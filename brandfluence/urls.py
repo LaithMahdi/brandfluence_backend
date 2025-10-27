3eb3a50bdf39971c6125b5e31b4d5fec37521bf9
LaithMahdi <mahdilaith380@gmail.com>
2025-10-27 20:01:01 +0100

    Add home page template and update URL routing for API information

--- brandfluence/urls.py
+++ brandfluence/urls.py
@@ -3,24 +3,22 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from schema_graph.views import Schema
-from django.http import JsonResponse
+from django.shortcuts import render
 from .schema import schema
 
 admin.site.site_header = "BrandFluence Admin"
 admin.site.site_title = "BrandFluence Admin Portal"
 admin.site.index_title = "Welcome to BrandFluence Admin Portal"
 
-def health_check(request):
-    """Simple health check endpoint"""
+def home(request):
+    """Home page with API information"""
     import django
-    return JsonResponse({
-        'status': 'ok',
+    return render(request, 'home.html', {
         'django_version': django.get_version(),
-        'message': 'BrandFluence API is running'
     })
 
 urlpatterns = [
-    path('', health_check, name='health_check'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path("schema/", Schema.as_view()),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
