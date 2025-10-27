3b9ceb2dd6f4646a12f921d5af8f4a5072c0df2a
LaithMahdi <mahdilaith380@gmail.com>
2025-10-27 12:34:27 +0100

    Refactor CategoryFilter and CategoryListQuery: Remove unused filters and implement DjangoFilterConnectionField for improved querying

--- category/category_filter.py
+++ category/category_filter.py
@@ -5,14 +5,6 @@ from .models import Category
 class CategoryFilter(FilterSet):
     """Filters for searching and filtering categories in GraphQL queries"""
     
-    name_contains = CharFilter(field_name='name', lookup_expr='icontains')
-    description_contains = CharFilter(field_name='description', lookup_expr='icontains')
-    is_active = BooleanFilter(field_name='is_active')
-    created_after = DateTimeFilter(field_name='created', lookup_expr='gte')
-    created_before = DateTimeFilter(field_name='created', lookup_expr='lte')
-    modified_after = DateTimeFilter(field_name='modified', lookup_expr='gte')
-    modified_before = DateTimeFilter(field_name='modified', lookup_expr='lte')
-    
     class Meta:
         model = Category
         fields = {
--- category/queries/category_list.py
+++ category/queries/category_list.py
@@ -1,12 +1,17 @@
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
 from ..models import Category
-from ..category_node import CategoryConnection
+from ..category_node import CategoryConnection, CategoryNode
+from ..category_filter import CategoryFilter
 
 
 class CategoryListQuery(graphene.ObjectType):
     """Query to get all categories with pagination and totalCount"""
     
-    all_categories = graphene.ConnectionField(CategoryConnection)
+    all_categories = DjangoFilterConnectionField(
+        CategoryNode,
+        filterset_class=CategoryFilter
+    )
     
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all().order_by('-created')
